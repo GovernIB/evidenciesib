90fef1bf8cfa258ac3ca01453de34c58fd74fade
Antoni Nadal <anadal@fundaciobit.org>
2024-09-25 12:35:53 +0200

    Afegir script SQL de dades amb els idiomes disponibles. #55

---
+++ scripts/bbdd/oracle/1.0/07_create_data_evidenciesib.sql
@@ -0,0 +1,8 @@
+
+
+INSERT INTO evi_idioma(idiomaid, nom, suportat, ordre) VALUES ('ca', 'Català', 1, 0);
+INSERT INTO evi_idioma(idiomaid, nom, suportat, ordre) VALUES ('es', 'Castellano', 1, 1);
+INSERT INTO evi_idioma(idiomaid, nom, suportat, ordre) VALUES ('en', 'English', 0, 2);
+
+
+
---
+++ scripts/bbdd/postgresql/1.0/02_create_data_evidenciesib.sql
@@ -0,0 +1,8 @@
+
+
+INSERT INTO evi_idioma(idiomaid, nom, suportat, ordre) VALUES ('ca', 'Català', true, 0);
+INSERT INTO evi_idioma(idiomaid, nom, suportat, ordre) VALUES ('es', 'Castellano', true, 1);
+INSERT INTO evi_idioma(idiomaid, nom, suportat, ordre) VALUES ('en', 'English', false, 2);
+
+
+
