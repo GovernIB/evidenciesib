751a17ea8d262b3e6d5dfbddcfbf667bf10c2e31
Antoni Nadal <anadal@fundaciobit.org>
2025-01-22 08:34:03 +0100

    Obrir Projecte  #38

--- evidenciesib-commons/src/main/java/es/caib/evidenciesib/commons/utils/StaticVersion.java
+++ evidenciesib-commons/src/main/java/es/caib/evidenciesib/commons/utils/StaticVersion.java
@@ -4,7 +4,7 @@ package es.caib.evidenciesib.commons.utils;
 */
 public interface StaticVersion {
 
-    public static final String VERSION="1.0.2";
+    public static final String VERSION="1.0.3";
 
     public static final String PROJECT_NAME="evidenciesib";
 
